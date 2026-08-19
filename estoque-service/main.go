be159ae3b72482791eeaa260bd693d223ae60e5b
Frederico Santos Saggio <saggiofrederico@gmail.com>
2026-08-19 16:43:03 -0300

    feat: Add docker compose

---
+++ estoque-service/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	connStr := os.Getenv("ESTOQUE_DATABASE_URL")
+	ctx := context.Background()
+	dbpool, err := pgxpool.New(ctx, connStr)
+
+	if err != nil {
+		log.Fatal("Error to create connection: ", err)
+	}
+
+	defer dbpool.Close()
+
+	if err := dbpool.Ping(ctx); err != nil {
+		log.Fatal("Error to access database: ", err)
+	}
+
+	log.Printf("Connected to PostgreSQL")
+}
