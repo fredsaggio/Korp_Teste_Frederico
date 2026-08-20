0b6fb117f477cab59eaaa2f199167b2f304cdf2a
Frederico Santos Saggio <saggiofrederico@gmail.com>
2026-08-20 15:53:26 -0300

    feat: initialize HTTP server in inventory service and document project implementation plan

--- estoque-service/cmd/main.go
+++ estoque-service/cmd/main.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"korp/estoque-service/internal/server"
+
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	ctx := context.Background()
 	connStr := os.Getenv("ESTOQUE_DATABASE_URL")
-	dbpool, err := pgxpool.New(ctx, connStr)
 
+	dbpool, err := pgxpool.New(ctx, connStr)
 	if err != nil {
-		log.Fatal("Error to create connection: ", err)
+		return fmt.Errorf("error to access database: %w", err)
 	}
 
 	defer dbpool.Close()
 
 	if err := dbpool.Ping(ctx); err != nil {
-		log.Fatal("Error to access database: ", err)
+		return fmt.Errorf("Error to access database: %w", err)
 	}
 
-	log.Printf("Connected to PostgreSQL")
+	httpServer := server.New(dbpool)
+
+	log.Printf("Estoque service running on port 5001!")
+
+	if err := httpServer.ListenAndServe(); err != nil {
+		return fmt.Errorf("error running HTTP server: %w", err)
+	}
+
+	return nil
 }
--- estoque-service/internal/server/server.go
+++ estoque-service/internal/server/server.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Server struct {
+	db *pgxpool.Pool
+}
+
+func New(db *pgxpool.Pool) *http.Server {
+	server := &Server{
+		db: db,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", server.health)
+
+	return &http.Server{
+		Addr:              ":5001",
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+}
+
+func (s *Server) health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status":"ok"}`))
+}
